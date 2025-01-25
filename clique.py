395288d37f84c21d71832698761aa6443fba60d8
berru-g <g.leberruyer@gmail.com>
2025-01-25 14:19:24 +0100

    vv

--- clique.py
+++ clique.py
@@ -3,36 +3,31 @@ import pyautogui
 import time
 import winsound
 
-# Minimise la fenêtre de la console
+
 kernel32 = ctypes.WinDLL('kernel32')
 user32 = ctypes.WinDLL('user32')
 
-SW_HIDE = 1  # 0 Cache la console
+SW_HIDE = 1 
 hWnd = kernel32.GetConsoleWindow()
 if hWnd:
     user32.ShowWindow(hWnd, SW_HIDE)
 
-# Décompte avant le lancement
 print("Tap To Earn Autoclick for pepememe.io")
 for i in range(10, 0, -1):
     print(f"d {i} ...")
     time.sleep(1)
 
-print("Début du programme.")
+print("Autoclick faster than my grandMa who has Parkinson ! ")
 
-# Boucle infinie
 while True:
-   # print(f"Boucle  {session + 1}")
 
-    # Répète 6 sessions de clics
     for session in range(7):
         print(f"--- Session {session + 1} ---")
         
-        # 200 clics dans cette session
         for click in range(1, 101):
             pyautogui.click(319, 813)# use coordonate
-            time.sleep(0.1)  # Pause de 0.1 seconde entre les clics
-            if click % 10 == 0:  # Afficher la progression tous les 10 clics
+            time.sleep(0.1)  
+            if click % 10 == 0: 
                 print(f"Clic {click}/100")
         
         print(f"Session {session + 1} terminée.")
@@ -41,7 +36,7 @@ while True:
         if session < 2:  # Pas de pause après la dernière session
             print("Pause ")
             for remaining in range(30, 0, -1):
-                if remaining % 10 == 0 or remaining <= 5:  # Afficher toutes les 10 secondes et les 5 dernières secondes
+                if remaining % 10 == 0 or remaining <= 5:  
                     print(f" {remaining} s")
                 time.sleep(1)
     print("Préparation...")
@@ -52,7 +47,6 @@ while True:
     winsound.Beep(440, 200)
     print("Boucle FINITO")
      
-    # Pause de 5 minutes après les 3 sessions avant de recommencer
     print("Pause de 300s")
     for remaining in range(300, 0, -1):  # 5 minutes = 300 secondes
         if remaining % 10 == 0 or remaining <= 5:  # Afficher toutes les 10 secondes et les 5 dernières secondes
